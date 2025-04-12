c3974f74706d266f59fe0ccc6ffb6885031d9644
jaruss1003 <jacob.a.russ11@gmail.com>
2025-04-11 20:41:55 -0400

    Create iris_data_analysis.py

---
+++ iris_data_analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the Iris dataset
+iris = sns.load_dataset('iris')
+
+# Show the first few rows of the dataset
+print(iris.head())
