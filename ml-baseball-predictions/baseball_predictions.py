a7d5795670c59e52610bf6bdd3c171b7c928955d
jaruss1003 <jacob.a.russ11@gmail.com>
2025-04-12 00:51:41 -0400

    cleaning

--- ml-baseball-predictions/baseball_predictions.py
+++ ml-baseball-predictions/baseball_predictions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # Path to data folder
-data_path = 'data/'
+data_path = 'data:/'
 
 # Load all CSVs
 files = [f for f in os.listdir(data_path) if f.endswith('.csv')]
